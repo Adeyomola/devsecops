dddd3ed2899d82bff5bc8134a1841490d470e72b
Adeyomola Kazeem <44479277+Adeyomola@users.noreply.github.com>
2023-04-30 19:08:04 +0100

    Create talisman.sh

---
+++ talisman.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+curl --silent  https://raw.githubusercontent.com/thoughtworks/talisman/master/global_install_scripts/install.bash \
+> /tmp/install_talisman.bash && /bin/bash /tmp/install_talisman.bash
